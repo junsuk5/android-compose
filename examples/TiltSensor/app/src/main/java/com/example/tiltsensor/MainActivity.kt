c49922dbd6dfde4a4e69f5be58d51127d69040be
Junsuk Oh <a811219@gmail.com>
2021-07-13 18:19:05 +0900

    TiltView를 Compose로 변경

--- examples/TiltSensor/app/src/main/java/com/example/tiltsensor/MainActivity.kt
+++ examples/TiltSensor/app/src/main/java/com/example/tiltsensor/MainActivity.kt
@@ -12,35 +12,42 @@ import android.util.Log
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.*
 import com.example.tiltsensor.ui.theme.TiltSensorTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // 화면이 꺼지지 않게 하기
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         // 화면이 가로 모드로 고정되게 하기
-        requestedOrientation = Activitynfo.SCREEN_ORIENTATION_LANDSCAPE
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
+
+        // 라이프사이클 관찰자 등록
+        lifecycle.addObserver(viewModel)
+
         setContent {
             TiltSensorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    AndroidView(modifier = Modifier.fillMaxSize(),
-                        factory = { context ->
-                            TiltView(context)
-                        },
-                        update = { view ->
-                        }
-                    )
+                    HomeScreen(viewModel)
                 }
             }
         }
@@ -48,8 +55,14 @@ class MainActivity : ComponentActivity() {
 }
 
 class MainViewModel(application: Application) : AndroidViewModel(application),
-    SensorEventListener,
-    LifecycleObserver {
+    SensorEventListener, LifecycleObserver {
+
+    private val _value0 = MutableStateFlow(0f)
+    val x: StateFlow<Float> = _value0
+
+    private val _value1 = MutableStateFlow(0f)
+    val y: StateFlow<Float> = _value1
+
     private val sensorManager by lazy {
         application.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
@@ -74,6 +87,8 @@ class MainViewModel(application: Application) : AndroidViewModel(application),
                 "MainViewModel",
                 "onSensorChanged x: ${event.values[0]}, y: ${event.values[1]}, z: ${event.values[2]}"
             )
+            _value0.value = event.values[0]
+            _value1.value = event.values[1]
         }
     }
 
@@ -84,7 +99,45 @@ class MainViewModel(application: Application) : AndroidViewModel(application),
 
 @Composable
 fun HomeScreen(viewModel: MainViewModel) {
+    val x = viewModel.x.collectAsState()
+    val y = viewModel.y.collectAsState()
 
+    TiltScreen(x = x.value, y = y.value)
+}
+
+@Composable
+fun TiltScreen(x: Float, y: Float) {
+    val yCoord = x * 20
+    val xCoord = y * 20
+
+    Canvas(modifier = Modifier.fillMaxSize()) {
+        val centerX = size.width / 2
+        val centerY = size.height / 2
+        // 바깥 원
+        drawCircle(
+            color = Color.Black,
+            radius = 100f,
+            center = Offset(centerX, centerY),
+            style = Stroke()
+        )
+        // 녹색 원
+        drawCircle(
+            color = Color.Green,
+            radius = 100f,
+            center = Offset(xCoord + centerX, yCoord + centerY),
+        )
+        // 가운데 십자가
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX - 20, centerY),
+            end = Offset(centerX + 20, centerY)
+        )
+        drawLine(
+            color = Color.Black,
+            start = Offset(centerX, centerY - 20),
+            end = Offset(centerX, centerY + 20)
+        )
+    }
 }
 
 @Preview(showBackground = true)
@@ -92,7 +145,7 @@ fun HomeScreen(viewModel: MainViewModel) {
 fun DefaultPreview() {
     TiltSensorTheme {
         Surface(color = MaterialTheme.colors.background) {
-            Greeting("Android")
+            TiltScreen(1.5f, 1.2f)
         }
     }
 }
