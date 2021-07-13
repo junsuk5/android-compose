8268424335ac2e648e4c11b4a1697752cf20ed15
Junsuk Oh <a811219@gmail.com>
2021-07-13 11:36:33 +0900

    실로폰 로직 작성

--- examples/Xylophone/app/src/main/java/com/example/xylophone/MainActivity.kt
+++ examples/Xylophone/app/src/main/java/com/example/xylophone/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.xylophone
 
+import android.app.Application
+import android.media.SoundPool
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -16,24 +20,51 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.ViewModel
 import com.example.xylophone.ui.theme.XylophoneTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             XylophoneTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    MainScreen()
+                    MainScreen(viewModel)
                 }
             }
         }
     }
 }
 
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val soundPool = SoundPool.Builder().setMaxStreams(8).build()
+    private val sounds = listOf(
+        soundPool.load(application.applicationContext, R.raw.do1, 1),
+        soundPool.load(application.applicationContext, R.raw.re, 1),
+        soundPool.load(application.applicationContext, R.raw.mi, 1),
+        soundPool.load(application.applicationContext, R.raw.fa, 1),
+        soundPool.load(application.applicationContext, R.raw.sol, 1),
+        soundPool.load(application.applicationContext, R.raw.la, 1),
+        soundPool.load(application.applicationContext, R.raw.si, 1),
+        soundPool.load(application.applicationContext, R.raw.do2, 1),
+    )
+
+    fun playSound(index: Int) {
+        soundPool.play(sounds[index], 1f, 1f, 0, 0, 1f)
+    }
+
+    override fun onCleared() {
+        soundPool.release()
+        super.onCleared()
+    }
+}
+
 @Composable
-fun MainScreen() {
+fun MainScreen(viewModel: MainViewModel) {
     val keys = listOf(
         Pair("도", Color.Red),
         Pair("레", Color(0xFFFF9800)),
@@ -52,7 +83,11 @@ fun MainScreen() {
         keys.forEachIndexed { index, key ->
             val padding = (index + 2) * 8   // 16, 24, 32, ...
             Keyboard(
-                modifier = Modifier.padding(top = padding.dp, bottom = padding.dp),
+                modifier = Modifier
+                    .padding(top = padding.dp, bottom = padding.dp)
+                    .clickable {
+                        viewModel.playSound(index)
+                    },
                 text = key.first,
                 color = key.second
             )
@@ -80,6 +115,6 @@ fun Keyboard(modifier: Modifier = Modifier, text: String, color: Color) {
 @Composable
 fun DefaultPreview() {
     XylophoneTheme {
-        MainScreen()
+//        MainScreen()
     }
 }
