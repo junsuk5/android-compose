efe95c01b8aa7af64a9841a8d86add3de377a46e
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-11-20 09:08:32 +0900

    chore: Android Studio Giraffe | 2022.3.1 Patch 4 업데이트

--- examples/MyWebBrowser/app/build.gradle.kts
+++ examples/MyWebBrowser/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.surivalcoding.mywebbrowser"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.surivalcoding.mywebbrowser"
